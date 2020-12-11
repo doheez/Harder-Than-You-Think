309cc9cf94da81c911e44770adca2aa33fbec82b
dohee <dohee121@naver.com>
2020-12-11 21:28:18 +0900

    힌트 창

--- app/src/main/java/com/example/nol/Rabbit.java
+++ app/src/main/java/com/example/nol/Rabbit.java
@@ -1,11 +1,16 @@
 package com.example.nol;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -13,6 +18,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Rabbit extends AppCompatActivity implements View.OnTouchListener {
     ImageView rabbit, cookie, correct;
+    Button prevBtn, listBtn, hintBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,6 +30,35 @@ public class Rabbit extends AppCompatActivity implements View.OnTouchListener {
         correct = (ImageView) findViewById(R.id.correct1);
 
         cookie.setOnTouchListener(this);
+
+        // 이전 단계
+        prevBtn = (Button) findViewById(R.id.rabbitPrev);
+        prevBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Enter.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        // 게임 목록
+        listBtn = (Button) findViewById(R.id.rabbitList);
+        listBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // 힌트 보기
+        hintBtn = (Button) findViewById(R.id.rabbitHint);
+        hintBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                makeDialog("화면에는 토끼가 한 마리만 있는 게 아닙니다.");
+            }
+        });
     }
 
     float oldXvalue;
@@ -71,5 +106,31 @@ public class Rabbit extends AppCompatActivity implements View.OnTouchListener {
             Log.d("viewTest", "x : " + event.getRawX() + " y : " + event.getRawY());
         }
         return true;
+    }
+
+    public void makeDialog(String text){
+        // 커스텀 다이얼로그를 정의하기위해 Dialog클래스를 생성한다.
+        Dialog dlg = new Dialog(this);
+
+        // 액티비티의 타이틀바를 숨긴다.
+        dlg.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // 커스텀 다이얼로그의 레이아웃을 설정한다.
+        dlg.setContentView(R.layout.custom_dialog);
+
+        // 커스텀 다이얼로그를 노출한다.
+        dlg.show();
+
+        // 커스텀 다이얼로그의 각 위젯들을 정의한다.
+        TextView hintText = (TextView)dlg.findViewById(R.id.hintText);
+        TextView hintOk = (TextView)dlg.findViewById(R.id.hintOk);
+
+        hintText.setText(text);
+        hintOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dlg.dismiss();
+            }
+        });
     }
 }
