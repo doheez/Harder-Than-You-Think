a26c7c45e756ae1a410b933f81e318a1ef815a19
dohee <dohee121@naver.com>
2020-12-11 19:27:37 +0900

    토끼에게 먹이 주기

--- app/src/main/java/com/example/nol/Enter.java
+++ app/src/main/java/com/example/nol/Enter.java
@@ -1,5 +1,6 @@
 package com.example.nol;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,7 +24,9 @@ public class Enter extends AppCompatActivity {
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(getApplicationContext(), Rabbit.class);
+                startActivity(intent);
+                finish();
             }
         });
 
---
+++ app/src/main/java/com/example/nol/Rabbit.java
@@ -0,0 +1,75 @@
+package com.example.nol;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Rabbit extends AppCompatActivity implements View.OnTouchListener {
+    ImageView rabbit, cookie, correct;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1_rabbit);
+
+        rabbit = (ImageView) findViewById(R.id.rabbit);
+        cookie = (ImageView) findViewById(R.id.cookie);
+        correct = (ImageView) findViewById(R.id.correct1);
+
+        cookie.setOnTouchListener(this);
+    }
+
+    float oldXvalue;
+    float oldYvalue;
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        int parentWidth = ((ViewGroup) v.getParent()).getWidth();    // 부모 View 의 Width
+        int parentHeight = ((ViewGroup) v.getParent()).getHeight();    // 부모 View 의 Height
+
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            // 뷰 누름
+            oldXvalue = event.getX();
+            oldYvalue = event.getY();
+            Log.d("viewTest", "oldXvalue : " + oldXvalue + " oldYvalue : " + oldYvalue);    // View 내부에서 터치한 지점의 상대 좌표값.
+            Log.d("viewTest", "v.getX() : " + v.getX());    // View 의 좌측 상단이 되는 지점의 절대 좌표값.
+            Log.d("viewTest", "RawX : " + event.getRawX() + " RawY : " + event.getRawY());    // View 를 터치한 지점의 절대 좌표값.
+            Log.d("viewTest", "v.getHeight : " + v.getHeight() + " v.getWidth : " + v.getWidth());    // View 의 Width, Height
+
+        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            // 뷰 이동 중
+            v.setX(v.getX() + (event.getX()) - (v.getWidth() / 2));
+            v.setY(v.getY() + (event.getY()) - (v.getHeight() / 2));
+
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            // 뷰에서 손을 뗌
+
+            if (v.getX() < 0) {
+                v.setX(0);
+            } else if ((v.getX() + v.getWidth()) > parentWidth) {
+                v.setX(parentWidth - v.getWidth());
+            }
+
+            if (v.getY() < 0) {
+                v.setY(0);
+            } else if ((v.getY() + v.getHeight()) > parentHeight) {
+                v.setY(parentHeight - v.getHeight());
+            }
+
+            // 토끼 글자 위에서 손을 뗌
+            if(event.getRawX()> 300 && event.getRawX() < 500 && event.getRawY() > 160 && event.getRawY() <290) {
+                correct.setVisibility(View.VISIBLE);
+            }
+            Log.d("viewTest", "x : " + event.getRawX() + " y : " + event.getRawY());
+        }
+        return true;
+    }
+}
